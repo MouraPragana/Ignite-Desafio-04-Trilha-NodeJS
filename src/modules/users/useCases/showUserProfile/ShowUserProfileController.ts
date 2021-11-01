41131de1e81176144938f83e1e9017d53a98574f
Mateus Moura <mateuspragana@gmail.com>
2021-11-01 14:05:18 -0300

    Concluindo a etapa de get user profile by ID

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -1,13 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { Request, Response } from "express";
 
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+    constructor(private showUserProfileUseCase: ShowUserProfileUseCase) { }
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { id } = request.body;
+        const user = this.showUserProfileUseCase.execute(id);
+        return response.status(200).json(user);
+    }
 }
 
 export { ShowUserProfileController };
--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -1,16 +1,21 @@
+/* eslint-disable prettier/prettier */
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ShowUserProfileUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) { }
 
-  execute({ user_id }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User {
+        const userExists = this.usersRepository.findById(user_id);
+        if (userExists) {
+            return userExists;
+        }
+        throw new Error("User doesnt exist");
+    }
 }
 
 export { ShowUserProfileUseCase };
