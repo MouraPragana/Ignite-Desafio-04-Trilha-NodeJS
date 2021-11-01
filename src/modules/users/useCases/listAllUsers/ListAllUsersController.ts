97f5c3b325bf9fe18593c05544cf444c10380169
Mateus Moura <mateuspragana@gmail.com>
2021-11-01 14:13:11 -0300

    Terminando o desafio dos useCases

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,13 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { id } = request.body;
+        const listUsers = this.listAllUsersUseCase.execute(id);
+        return response.status(200).json(listUsers);
+    }
 }
 
 export { ListAllUsersController };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,16 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) { }
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const userExists = this.usersRepository.findById(user_id);
+        if (userExists) {
+            if (userExists.admin === true) {
+                return this.usersRepository.list()
+            }
+        }
+        throw new Error("You are not a admin")
+    }
 }
 
 export { ListAllUsersUseCase };
