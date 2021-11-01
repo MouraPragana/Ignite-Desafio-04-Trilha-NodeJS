6c5ba6a88d9136a85c7ee266d8742653239e1389
Mateus Moura <mateuspragana@gmail.com>
2021-11-01 13:59:42 -0300

    Continuando a resolução de desafios - useCases - createuser, turn user as admin

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -1,13 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { Response, Request } from "express";
 
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+    constructor(private createUserUseCase: CreateUserUseCase) { }
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { name, email } = request.body
+        this.createUserUseCase.execute({ name, email })
+        return response.status(200).json({ msg: "User created!" })
+    }
 }
 
 export { CreateUserController };
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -1,13 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { Request, Response } from "express";
 
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+    constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) { }
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { id } = request.body;
+        this.turnUserAdminUseCase.execute({ user_id: id });
+        return response.status(201).json({ msg: "User updated to admin" });
+    }
 }
 
 export { TurnUserAdminController };
